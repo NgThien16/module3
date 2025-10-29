640176bc33db94bd960f273663753d0e22446325
NgThien16 <thiennngcd220423@fpt.edu.vn>
2025-10-29 21:49:55 +0700

    db

---
+++ sql/database_overview.sql
@@ -0,0 +1,23 @@
+create database demo;
+use demo;
+-- tao bang Student
+create table Student(
+id int,
+name varchar(200),
+age int,
+country varchar(50)
+);
+--tao bang class
+create table Class(
+id int,
+name varchar(200)
+);
+#tao bang teacher
+create table Teacher(
+id int,
+name varchar(200),
+age int,
+country varchar(50)
+);
+
+
